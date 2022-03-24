1b37140780f7c441009b80808c116360e9d42cd1
Rayen Bedoui <altarius1998@gmail.com>
2022-03-24 02:45:17 +0100

    last commit 52:00

--- Chat Application/MVVM/ViewModel/MainViewModel.cs
+++ Chat Application/MVVM/ViewModel/MainViewModel.cs
@@ -1,19 +1,51 @@
-﻿using Chat_Application.MVVM.Model;
+﻿using Chat_Application.Core;
+using Chat_Application.MVVM.Model;
 using System;
 using System.Collections.ObjectModel;
 
 namespace Chat_Application.MVVM.ViewModel
 {
-    internal class MainViewModel
+    class MainViewModel : ObservableObjects
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        /* Commands */
+        public RelayCommand SendCommand { get; set; }
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set { _selectedContact = value; 
+            OnPropertyChanged();}
+        }
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set { _message = value;
+                OnPropertyChanged();
+            }
+            
+            
+        }
 
         public MainViewModel()
             {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
 
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    Message = Message,
+                    FirstMessage = false
+                }) ;
+                Message = "";
+            });
             Messages.Add(new MessageModel
             {
                 Username = "Allison",
